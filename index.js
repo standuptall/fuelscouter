8714ed4fad409444e9dc326503b141925b3a3cb9
Alberto <a.zichittella@kfi.it>
2021-09-27 11:16:28 +0200

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'https://standuptall.herokuapp.com/';
+const hostname = '127.0.0.1';
 const port = 443;
 
 const server = http.createServer((req, res) => {
