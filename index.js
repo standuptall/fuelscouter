7f04b4d5297fbcf17c880102889aa5b8368cbafb
Alberto <a.zichittella@kfi.it>
2021-09-27 11:14:27 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = 'https://standuptall.herokuapp.com/';
+const port = 443;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
